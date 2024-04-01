b8ab8ffde6cddfea6207fcd2e7ae459e4f81f6a8
Balazs Gonter <gonter.balazs@gmail.com>
2024-04-01 14:08:13 +0200

    Exercise 2-6 - Implement all endpoints
    * Get all persons
    * Get person by id
    * Delete person by id
    * Info endpoint with date and persons length
    * Create person

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -25,12 +25,70 @@ const persons = [
   },
 ];
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000);
+};
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/info", (request, response) => {
+  const htmlResponse = `<div>Phonebook has info for ${
+    persons.length
+  } people</div><br/><div>${new Date()}</div>`;
+  response.send(htmlResponse);
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => {
+    console.log(person.id, typeof person.id, id, typeof id, person.id === id);
+    return person.id === id;
+  });
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number missing. Please specify both!",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "This name already exists in the Phonebook. Please add a new one!",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3002;
